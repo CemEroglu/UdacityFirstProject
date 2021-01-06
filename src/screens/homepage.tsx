5da5fbea944324d2ba5dcb5100eb9ca86935f117
TURKCELL\tcceroglu <cem.eroglu@turkcell.com.tr>
2021-01-06 18:22:03 +0300

    url of images updated

--- src/screens/homepage.tsx
+++ src/screens/homepage.tsx
@@ -14,6 +14,40 @@ const HomePage = () => {
             {bookdata.map((item: any) => {
                 return <li>{item.title}</li>
             })}
+
+
+            <div>denemeee</div>
+            {bookdata.filter((item: any) => item.shelf === 'read').map((item: any) => {
+
+                return <div>
+
+                    <div className="book">
+                        <div className="book-top">
+                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url('+item.imageLinks.thumbnail+')' }}></div>
+                            <div className="book-shelf-changer">
+                                <select>
+                                    <option value="move" disabled>Move to...</option>
+                                    <option value="currentlyReading">Currently Reading</option>
+                                    <option value="wantToRead">Want to Read</option>
+                                    <option value="read">Read</option>
+                                    <option value="none">None</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div className="book-title">{item.title}</div>
+                        <div className="book-authors">{item.authors.map((item: any) => {
+                            return item + ','
+                        })}</div>
+                    </div>
+
+
+
+
+                </div>
+
+
+            })}
+
         </div>
     );
 }
