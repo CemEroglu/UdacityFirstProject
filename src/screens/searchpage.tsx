1b98e778af174cc6a31cdc1a803080b251774647
TURKCELL\tcceroglu <cem.eroglu@turkcell.com.tr>
2021-01-09 22:47:20 +0300

    listed books on search written

--- src/screens/searchpage.tsx
+++ src/screens/searchpage.tsx
@@ -4,12 +4,18 @@ import '../App.css'
 import { updateObjectBindingPattern } from "typescript";
 
 import { useHistory } from 'react-router-dom'
+import { strict } from "assert";
 
 const SearchPage = () => {
 
     const history = useHistory();
     const [bookdata, setbookdata] = useState([]);
     const [searchWord, setsearchWord] = useState('');
+
+    const [currentlyReadingBooks, setcurrentlyReadingBooks] = useState([]as any);
+    const [wanttoReadBooks, setwanttoReadBooks] = useState([]as any);
+    const [readBooks, setreadBooks] = useState([] as any);
+
     useEffect(() => {
         if (searchWord) {
             BooksAPI.search(searchWord).then(
@@ -18,7 +24,7 @@ const SearchPage = () => {
                     if (data.hasOwnProperty('error')) {
                         setbookdata([])
                     }
-                    else{
+                    else {
                         setbookdata(data)
                     }
                 })
@@ -28,6 +34,28 @@ const SearchPage = () => {
         // })
 
     }, [searchWord])
+    useEffect(() => {
+        BooksAPI.getAll().then((data) => {
+            console.log(data)
+            let readBooksID=[] as any;
+            let curentlyreadBooksID=[] as any;
+            let wanttoreadBooksID=[] as any;  
+            data.map((item: any) => {
+                if (item.shelf === 'read') {
+                    readBooksID.push(item.id)
+                }
+                if (item.shelf === 'currentlyReading') {
+                    curentlyreadBooksID.push(item.id)
+                }
+                if (item.shelf === 'wantToRead') {
+                    wanttoreadBooksID.push(item.id)
+                }
+            })
+            setreadBooks(readBooksID)
+            setcurrentlyReadingBooks(curentlyreadBooksID)
+            setwanttoReadBooks(wanttoreadBooksID)
+        })
+    }, [])
     return (
 
 
@@ -56,32 +84,39 @@ const SearchPage = () => {
 
                     {bookdata ? (
                         bookdata.map((item: any) => {
-                            if(item.title &&item.imageLinks &&item.imageLinks.thumbnail && item.title && item.authors ){
-                                
-                            return <li>
+                            if (item.title && item.imageLinks && item.imageLinks.thumbnail && item.title && item.authors) {
 
-                                <div className="book">
-                                    <div className="book-top">
-                                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url(' + item.imageLinks.thumbnail + ')' }}></div>
-                                        <div className="book-shelf-changer">
-                                            <select value={item.shelf} onChange={(event: any) => {
-                                                BooksAPI.update(item, event.target.value)
-                                            }}>
-                                                <option value="move" disabled>Move to...</option>
-                                                <option value="currentlyReading">Currently Reading</option>
-                                                <option value="wantToRead">Want to Read</option>
-                                                <option value="read">Read</option>
-                                                <option value="none">None</option>
-                                            </select>
+                                return <li>
+
+                                    <div className="book">
+                                        <div className="book-top">
+                                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url(' + item.imageLinks.thumbnail + ')' }}></div>
+                                            <div className="book-shelf-changer">
+                                                <select value={
+                                                    
+                                                    readBooks.includes(item.id)?'read':(
+                                                    currentlyReadingBooks.includes(item.id)?'currentlyReading':(
+                                                        wanttoReadBooks.includes(item.id)?'wantToRead':'none'
+                                                    ))
+                                                } onChange={(event: any) => {
+                                                    BooksAPI.update(item, event.target.value)
+                                                }}>
+                                                    <option value="move" disabled>Move to...</option>
+                                                    <option value="currentlyReading">Currently Reading</option>
+                                                    <option value="wantToRead">Want to Read</option>
+                                                    <option value="read">Read</option>
+                                                    <option value="none">None</option>
+                                                </select>
+                                            </div>
                                         </div>
+                                        <div className="book-title">{item.title}</div>
+                                        {item.authors ?
+                                            <div className="book-authors">{item.authors.map((item: any) => {
+                                                return item + ','
+                                            })}</div> : ''}
                                     </div>
-                                    <div className="book-title">{item.title}</div>
-                                    {item.authors ?
-                                        <div className="book-authors">{item.authors.map((item: any) => {
-                                            return item + ','
-                                        })}</div> : ''}
-                                </div>
-                            </li>}
+                                </li>
+                            }
                         }))
                         : ''}
 
